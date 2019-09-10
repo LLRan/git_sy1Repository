5568a454883fde1d537f96d55f88d47600b2d855
Yanran <2405326414@qq.com>
2019-09-10 15:09:10 +0800

    小圆提交新建项目

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,7 +1,5 @@
-public class Helloxz {
-    public static void main(String[] args) {
+public class Hello {
+    public static void main1(String[] args) {
         System.out.println("lll1531465464f64sd6f4s6df6dasfsdafr");
-
-
     }
 }
